b8e76d8b883878710ac99960d4885c4ca00adbe3
Abby Terzis <terzis@wisc.edu>
2022-10-20 07:26:05 -0500

    gives average output

--- school.sh
+++ school.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d , -f 7 | {sum=0; total=0; while read n; do sum=$(($sum + $n)); total=$(($count + 1)); done; average=$(echo "scale = 4; (($sum/$total))" | bc); echo $average;}

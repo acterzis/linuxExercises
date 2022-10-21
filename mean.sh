3b16beaa5f81aa59910110a380d60d2ef2df45a3
Marwa Eltgani <eltgani@wisc.edu>
2022-10-21 16:45:26 -0500

    draft

--- mean.sh
+++ mean.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+column = $1
+
+echo "usage:mean.sh <column> [file.csv]"
+
+if [[$# -eq 2]]; then
+    file = $2
+elif [[$# -eq 1]]; then
+     file = /dev/stdin
+else
+    echo "none"
+fi
+
+cut $file -d, -f $column |
+    tail -n + 2 |
+    {sum=0;
+     total=0;
+     while read value; do
+	 sum=$(($sum + $value));
+	 total=$(($total + 1));
+     done;
+     average=$(echo "scale = 4; $sum/$total" | bc);
+     echo $average;
+}

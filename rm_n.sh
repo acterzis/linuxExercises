1264df290fc37d5c7c8a8b89fa7c0515c46689e6
Anna Hayes <achayes@wisc.edu>
2022-10-21 16:48:43 -0500

    removes files from a specified directory of a specified size

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+if [[ $# -ne 2 ]]; then #loop checks if user input 2 values, if not show error
+    echo "usage: $0 <dir> <n>" 1>&2 # writes error message to stderr
+    exit 0
+fi
+
+cd "$1" #changes dirctory to users input 
+
+file=$(find . -type f -size +$2c) #find files in cd with user size in bytes 'c'
+
+for f in $file # loops through files and deletes them 
+do
+    rm $f
+done

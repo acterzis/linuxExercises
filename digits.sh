b192dc527abf58df583d032e5e5e95cf4d39c64b
Anna Hayes <achayes@wisc.edu>
2022-10-21 16:54:43 -0500

    sum digits in 1000 to 2000 comprised of digits in set {0,1}

--- digits.sh
+++ digits.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-re='\b[^2-9]{1,4}\b'
-sum=0
-for r in {1000..2000}
+re='\b[^2-9]{1,4}\b' #regular expression for finding digits that are  2-9
+sum=0 # initalize sum
+for r in {1000..2000} #loop through range of values
 do
-    if [[ $r =~ $re ]]; then
-	sum=$(($sum + $r))
+    if [[ $r =~ $re ]]; then # finds if the index r matches the re
+	sum=$(($sum + $r)) # if so, add the index to current sum
     fi
 done
 echo $sum

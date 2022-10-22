683a2719d3b292fdf90fcd91e01523e913caed65
Marwa Eltgani <eltgani@wisc.edu>
2022-10-21 22:15:00 -0500

    while loop

--- five_dirs.sh
+++ five_dirs.sh
@@ -4,13 +4,17 @@ mkdir five
 cd five
 
 for i in {1..5};do
-	 mkdir "dir${i}"
-	 for j in {1..4};do
-		  for l in {1..$j};do
-			   echo $j >>"dir${i}/files${j}"
-		  done
+	 mkdir "dir${i}" 
+   for j in {1..4};do
+		 file="dir${i}/files${j}"
+		 l=1
+		 while(($l<=$j));do
+			 echo $j >> $file
+			 let "l++"
+		 done;
 	 done
-done
+
 	 
-	      
+ done
+
 	 
